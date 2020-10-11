f5db490f30c9d7fd9897019ea441a4cea1403b94
Amy Paschke <pasch049@umn.edu>
2020-10-11 16:55:58 -0500

    added additional JS code for functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,51 @@
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
+// Assignment Code, this applies to the bottom fuction
+let generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
+  let password = generatePassword(); //create this
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button- when user clicks button, run function
 generateBtn.addEventListener("click", writePassword);
 
-//prompts/confirms for the password
-let passwordLength = prompt("Please indicate the length you want for your password (between 8-128");
-  if (passwordLength === a); {
-    console.log(stuff);
-  } else {
-    alert("Please choose a number between 8-128.");
+
+/*--------------------------------------- */
+
+let inputValid = 1;
+// the needed prompts and confirms for the password
+do {
+  let passwordString = prompt("Please indicate the length you want for your password (between 8-128.");
+    if (passwordString.isNan()) {
+      alert("Please enter a number between 8-128.");
+    } else {
+
+      inputValid = 0;
+  }
 }
 
-let lowercaseValues = "abcdefghijklmnopqrstuvwxyz";
-let uppercaseValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let specialValues = "";
+while 
+
+
+//changes user input from string to intiger
+let passwordLength = parseInt(passwordString);
+
+/*variable used to generate password based on previous criteria
+0-25 is lowercase, 26-51 is upper case, 52 - 83 are special characters*/
+const passwordValues = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !#$%&/'()*+,-./:;<=>?@[]^_`{|}~";
+
+//add in an array to the i <= to account for the length given in the prompt
+//for loop used to make the password
+for (let i = 0; i <= passwordLength; i++) {
+  password = password + passwordValues.charAt(Math.floor(Math.random * Math.floor(passwordValues.length - 1))); //this only searches within the const, and does not take into account the selections the selector can make
+    
+}
+
+password += //function call
+
+//randomize how math.random works and then assign a random number to a variable? 
+
