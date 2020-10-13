6412a5990c68614dfbdc60c5ec38567071bd7d6e
Amy Paschke <pasch049@umn.edu>
2020-10-13 11:56:48 -0500

    reworked functions and added user choice for password generation

--- Assets/script.js
+++ Assets/script.js
@@ -25,28 +25,12 @@ function copyPassword() {
 
 /*--------------------------------------- */
 
-//function to generate password characters, lacking choice
-function passEverything(length) {
-  let password = '';
-  let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&/'()*+,-./:;<=>?@[]^_`{|}~";
-  let charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
-     password += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return password;
-}
+let lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+let passwordNumber = "1234567890".split("");
+let special = " !#$%&/'()*+,-./:;<=>?@[]^_`{|}~".split("");
 
-
-//create the generate password function--
-function generatePassword() {
-
-  let inputValid = 1;
-
-  let passwordLength = null;
-
-  /*changes user input from string to intiger
-  let passwordLength = parseInt(passwordString); */
-    
+function getPasswordOptions() {
   do {
     let passwordString = prompt("Please indicate the length you want for your password between 8-128 characters.");
       if (isNaN(passwordString)) {
@@ -61,31 +45,102 @@ function generatePassword() {
       }
     } while (inputValid === 1);
 
-  //inside the  do/while statement the inputValid changes from 1 to 0 once a number between 8 - 128 is selected, which is what this if statement is looking for
-  if (inputValid === 0) {
+  let hasSpecialCharacters = confirm("Do you want special character?");
+  let hasLowercaseCharacters = confirm("Do you want lowercase characters?");
+  let hasUppercaseCharacters = confirm("Do you want uppercase characters?");
+  let hasNumericCharacters = confirm("Do you want numbers?");
 
-   passEverything();
-      
-    }
+  if (hasSpecialCharacters === false && hasLowercaseCharacters === false && hasNumericCharacters === false && hasUppercaseCharacters === false) {
+    alert("Please confirm at least one option for your password.");
+    return;
+  }
+
+  const passwordOptions = {
+    passwordLength: passwordLength,
+    hasLowercaseCharacters: hasLowercaseCharacters,
+    hasUppercaseCharacters: hasUppercaseCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasSpecialCharacters: hasSpecialCharacters
+  }
+
+  return passwordOptions;
 }
+
+function getRandom(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  let randomElement = arr[randomIndex];
+  return randomElement;
+}
+
+
+
+//function to generate password characters, lacking choice
+function passEverything(length) {
+  let password = '';
+  let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&/'()*+,-./:;<=>?@[]^_`{|}~";
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+     password += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return password;
+}
+
+
+//create the generate password function--
+function generatePassword() {
+  let result = [];
+  let possibleCharacters = [];
+  let guaranteedCharacters = [];
+  let options = getPasswordOptions();
+
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(special);
+    guaranteedCharacters.push(getRandom(special));
+  }
+
+  if (options.hasLowercaseCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowercase);
+    guaranteedCharacters.push(getRandom(lowercase));
+  }
+
+  if (options.hasUppercaseCharacters) {
+    possibleCharacters = possibleCharacters.concat(uppercase);
+    guaranteedCharacters.push(getRandom(uppercase));
+  }
+
+  if (options.hasNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(passwordNumber);
+    guaranteedCharacters.push(getRandom(passwordNumber));
+  }
+
+  for (let i = 0; i < options.passwordLength; i++) {
+    let randomCharacter = getRandom(possibleCharacters);
+    result.push(randomCharacter);
+  }
+  for (let kittens = 0; kittens < guaranteedCharacters.length; kittens++) {
+    result[kittens] = guaranteedCharacters[kittens];    
+  }
+  return result.join("");
+}
+
 
 
 
 /*variables used to generate password based on previous criteria 
 function lowercaseCharacter() {
-  let lowercase = "abcdefghijklmnopqrstuvwxyz";
+  
 }
 
 function uppercaseCharacter() {
-  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
 }
 
 function numberCharacter() {
-  let passwordNumber = "1234567890";
+  
 }
 
 function specialCharacter() {
-  let special = " !#$%&/'()*+,-./:;<=>?@[]^_`{|}~";
+  
 }
 
 //add in an array to the i <= to account for the length given in the prompt
